68f07df9c091ad15f15d11412f7400155bc9d974
Bashkar <hemalatha.bashkar@capgemini.com>
2024-12-23 10:18:40 +0530

    Added Practised programs

---
+++ print.py
@@ -0,0 +1,6 @@
+# Print statement to display output of the program
+
+var = 100
+print(var)
+print(28)  # directly printing output
+print(10 / 2 + 3) # using expressions
---
+++ string.py
@@ -0,0 +1,39 @@
+string = "Just do it!"
+print(string[10])
+print(string[5:7])
+print(string[8:])
+print(string[:4])
+slice = string[5:]
+str2 = "Don't "
+print(str2 + slice)
+
+
+
+#Escape sequence
+
+var = 10.8
+print(type(var))
+print(str(var) + "is a float")
+print("\"Hello, I'm hema, it's nice to meet you!\"")
+
+
+#triangle
+
+print("*******\n *****\n  ***\n   *")
+
+
+#input function
+
+name = input("What is your name?")
+quest = input("What is your quest?")
+color = input("What is your favorite color?")
+
+print("So, your name is " + name  +  ", your quest is " + quest + ", your facorite color is " + color + ".")
+
+
+# int(), float() function
+
+user_int = int(input("enter a number "))
+print(10 + user_int)
+print(type(user_int))
+
